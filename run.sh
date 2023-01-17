28df0929e412f4b08498f4924701f5676eee55ab
xyz <xyz@xyzrobotics.com>
2023-01-17 13:41:16 +0800

    fix: output uid gid

--- run.sh
+++ run.sh
@@ -10,9 +10,13 @@ if [ -z $1 ]; then
     gosu bproc /bin/bash -c "python $PROJECT_DIR/examples/datasets/deep-tote/gen_masks.py"
     gosu bproc /bin/bash -c "python $PROJECT_DIR/examples/datasets/deep-tote/gen_annotations.py"
 else
+    LOCAL_UID=$(stat -c %u $1)
+    LOCAL_GID=$(stat -c %u $1)
     # use custom configutation
     gosu bproc /bin/bash -c "blenderproc run $PROJECT_DIR/examples/datasets/deep-tote/custom.py --config $1"
     gosu bproc /bin/bash -c "python $PROJECT_DIR/examples/datasets/deep-tote/gen_masks.py --config $1"
     gosu bproc /bin/bash -c "python $PROJECT_DIR/examples/datasets/deep-tote/gen_annotations.py --config $1"
+    chown -R $LOCAL_UID:$LOCAL_GID output
 fi
+
 
